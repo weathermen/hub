ac148f4ae0cb1d417c4b0d0e13cdf23627e0857a
Tom Scott <tscott@weblinc.com>
2018-11-22 14:59:17 -0500

    Basic UI and API for the Soundstorm Hub

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -5,14 +5,36 @@ class InstancesController < ApplicationController
 
   def create
     @instance = Instance.find_or_create_by!(host: instance_params[:host])
-    @instance.update!(version: instance_params[:version])
 
-    render json: @instance, status: :created
+    if @instance.update(instance_params)
+      flash[:notice] = "Saved instance '#{@instance.host}'"
+      head status: :created
+    else
+      flash[:alert] = "Instance could not be saved."
+      head status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @instance = Instance.find(params[:id])
+
+    if @instance.destroy
+      flash[:notice] = "Instance '#{@instance.host}' has been deleted."
+    else
+      flash[:alert] = "Error destroying instance '#{@instance.host}'"
+    end
+
+    redirect_to root_url
   end
 
   private
 
   def instance_params
     params.require(:instance).permit(:host, :version)
+  end
+
+  def set_headers
+    headers['X-Success'] = flash[:notice]
+    headers['X-Error'] = flash[:alert]
   end
 end
